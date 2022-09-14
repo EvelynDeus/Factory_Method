d7ca79ff7f6ccdcb5e95fc3bbfa5287fc69a25c2
diana <diana@DESKTOP-JUCAL78>
2022-09-14 10:22:34 +0600

    Добавьте файлы проекта.

---
+++ Factory_Method/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    
+    interface ITariff
+    {
+        double AmountOfTlectricity { get; set; }
+
+        double GetCalculation(double amount);
+    }
+
+
+    abstract class TariffFactory
+    {
+        protected ITariff tariff;
+        public abstract void Cout(double amount);
+    }
+
+    class FirstTariffFactory: TariffFactory
+    {
+        public override void Cout(double amount)
+        {
+            tariff = new FirsType();
+            Console.WriteLine(tariff.GetCalculation(amount));
+        }
+    }
+
+    class SecondTariffFactory: TariffFactory
+    {
+        public override void Cout(double amount)
+        {
+            tariff = new SecondType();
+            Console.WriteLine(tariff.GetCalculation(amount));
+        }
+    }
+
+    class ThirdTariffFactory : TariffFactory
+    {
+        public override void Cout(double amount)
+        {
+            tariff = new ThirdType();
+            Console.WriteLine(tariff.GetCalculation(amount));
+        }
+    }
+
+    class FirsType: ITariff
+    {
+        public double AmountOfTlectricity { get; set; }
+
+        public double GetCalculation(double amount)
+        {
+            return amount * 5.2;
+        }
+    }
+
+    class SecondType: ITariff
+    {
+        public double AmountOfTlectricity { get; set; }
+
+        public double GetCalculation(double amount)
+        {
+            return amount * 7.5;
+        }
+
+    }
+
+    class ThirdType : ITariff
+    {
+        public double AmountOfTlectricity { get; set; }
+
+        public double GetCalculation(double amount)
+        {
+            return amount * 10.3;
+        }
+    }
+
+    class Flat
+    {
+        public double Elek { get; set; }
+    }
+}
---
+++ Factory_Method/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+
+namespace Factory_Method
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            TariffFactory factory;
+            List<Flat> flats = new List<Flat>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите колличество используемой электроэнергии для квартиры {i+1}");
+                double elect = Convert.ToDouble(Console.ReadLine());
+                flats.Add(new Flat { Elek = elect });
+            }
+
+
+            foreach(var flat in flats)
+            {
+                if (flat.Elek <= 2500)
+                {
+                    Console.WriteLine("Расчет по первому тарифу");
+                    factory = new FirstTariffFactory();
+                    factory.Cout(flat.Elek);
+                }
+                else if(flat.Elek <= 5750)
+                {
+                    Console.WriteLine("Расчет по второму тарифу");
+                    factory = new SecondTariffFactory();
+                    factory.Cout(flat.Elek);
+                }
+                else
+                {
+                    Console.WriteLine("Расчет по третьему тарифу");
+                    factory = new ThirdTariffFactory();
+                    factory.Cout(flat.Elek);
+                }
+            }
+        }
+    }
+}
